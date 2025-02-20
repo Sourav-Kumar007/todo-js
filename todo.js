bf463cb7f5e72ee91581fda2f2eb49f0326c6d0e
Sourav Roy <souravkumarroy813965@gmail.com>
2025-02-20 09:42:59 +0600

    delete button added everywhere

--- todo.js
+++ todo.js
@@ -28,7 +28,7 @@
 // listContainer.addEventListener("click", function (e) {
 //    e.target.style.color = "green";
 //    e.target.classList.add("completed");
-//   }); 
+//   });
 
 // //  function allCompleted()
 // //  {
@@ -37,17 +37,17 @@
 // //     {
 // //         console.log(e.target.value);
 // //     }
-// //  } 
+// //  }
 // function allCompleted() {
 //     let tasks = listContainer.getElementsByTagName("li");
 //     let completedTasks = [];
-    
-//     // for (let task of tasks) { 
-//     //     if (task.classList.contains("completed")) { 
+
+//     // for (let task of tasks) {
+//     //     if (task.classList.contains("completed")) {
 //     //         completedTasks.push(task);
 //     //     }
 //     // }
-    
+
 //     // let newContainer = document.getElementById("all-task");
 //     // completedTasks.forEach(task => newContainer.appendChild(task));
 
@@ -56,10 +56,10 @@
 
 //     for(let task of tasks)
 //     {
-//         if (task.classList.contains("completed")) { 
+//         if (task.classList.contains("completed")) {
 //                     continue;
 //                 }
-//                 else 
+//                 else
 //                 {
 //                     listContainer.removeChild(task);
 //                 }
@@ -79,49 +79,42 @@
 
 const arrayList = [];
 const obj = {
-  id : Date.now(),
-  task : "dummy",
-  marked : true
+  id: Date.now(),
+  task: "dummy",
+  marked: true,
 };
 
-
-document.querySelector('#input-box').addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
+document.querySelector("#input-box").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
     taskAdd(1);
   }
 });
 
-function taskAdd(f)
-{
-  if(f == 1)
-  {
+function taskAdd(f) {
+  if (f == 1) {
     const newInput = document.getElementById("input-box");
-  const newTask = Object.create(obj);
-  if(newInput.value == "")
-  {
-    return ;
-  }
-  newTask.task = newInput.value ;
-  newTask.marked = false;
-  newTask.id = Date.now();
-  newInput.value = "";
-  arrayList.push(newTask);
+    const newTask = Object.create(obj);
+    if (newInput.value == "") {
+      return;
+    }
+    newTask.task = newInput.value;
+    newTask.marked = false;
+    newTask.id = Date.now();
+    newInput.value = "";
+    arrayList.push(newTask);
   }
   const listContainer = document.getElementById("all-task");
   listContainer.innerHTML = "";
-  for(let i=0; i<arrayList.length; i++)
-  {
-    if(arrayList[i] === undefined)
-    {
+  for (let i = 0; i < arrayList.length; i++) {
+    if (arrayList[i] === undefined) {
       continue;
     }
 
     let li = document.createElement("li");
     li.innerHTML = arrayList[i].task;
-    li.setAttribute("id" , `${arrayList[i].id}`);
-    if(arrayList[i].marked)
-    {
-      li.style.backgroundColor = '#C0E49A';
+    li.setAttribute("id", `${arrayList[i].id}`);
+    if (arrayList[i].marked) {
+      li.style.backgroundColor = "#C0E49A";
     }
 
     let deleteBtn = document.createElement("button");
@@ -130,15 +123,14 @@ function taskAdd(f)
     console.log(arrayList[i].task);
     listContainer.appendChild(li);
 
-
-    li.querySelector("button").addEventListener("click" , remove);
-    function remove()
-    {
-      for(j=0; j<arrayList.length; j++)
-      {
+    li.querySelector("button").addEventListener("click", remove);
+    function remove() {
+      for (j = 0; j < arrayList.length; j++) {
         //console.log(arrayList[j].id , Number(li.getAttribute("id")));
-        if(arrayList[j] !== undefined && arrayList[j].id === Number(li.getAttribute("id")))
-        {
+        if (
+          arrayList[j] !== undefined &&
+          arrayList[j].id === Number(li.getAttribute("id"))
+        ) {
           delete arrayList[j];
           break;
         }
@@ -146,103 +138,195 @@ function taskAdd(f)
       li.remove();
     }
   }
-  listContainer.addEventListener("click" , function(e){
-    e.target.style.backgroundColor = '#C0E49A';
-    for(j=0; j<arrayList.length; j++)
-      {
-        //console.log(arrayList[j].id , Number(li.getAttribute("id")));
-        if(arrayList[j] !== undefined && arrayList[j].id === Number(e.target.getAttribute("id")))
-        {
-          arrayList[j].marked = true;
-          break;
-        }
+  listContainer.addEventListener("click", function (e) {
+    e.target.style.backgroundColor = "#C0E49A";
+    for (j = 0; j < arrayList.length; j++) {
+      //console.log(arrayList[j].id , Number(li.getAttribute("id")));
+      if (
+        arrayList[j] !== undefined &&
+        arrayList[j].id === Number(e.target.getAttribute("id"))
+      ) {
+        arrayList[j].marked = true;
+        break;
       }
+    }
   });
   //console.log(arrayList);
 }
 
-document.querySelector('#btn1').addEventListener('click', (e)=> {
+document.querySelector("#btn1").addEventListener("click", (e) => {
   e.target.style.backgroundColor = "red";
-  document.querySelector('#btn2').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn3').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn4').style.backgroundColor = '#5c67f2';
-} );
+  document.querySelector("#btn2").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn3").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn4").style.backgroundColor = "#5c67f2";
+});
 
-document.querySelector('#btn2').addEventListener('click', (e)=> {
+document.querySelector("#btn2").addEventListener("click", (e) => {
   e.target.style.backgroundColor = "red";
-  document.querySelector('#btn1').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn3').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn4').style.backgroundColor = '#5c67f2';
-} );
+  document.querySelector("#btn1").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn3").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn4").style.backgroundColor = "#5c67f2";
+});
 
-document.querySelector('#btn3').addEventListener('click', (e)=> {
+document.querySelector("#btn3").addEventListener("click", (e) => {
   e.target.style.backgroundColor = "red";
-  document.querySelector('#btn1').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn2').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn4').style.backgroundColor = '#5c67f2';
-} );
+  document.querySelector("#btn1").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn2").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn4").style.backgroundColor = "#5c67f2";
+});
 
-document.querySelector('#btn4').addEventListener('click', (e)=> {
+document.querySelector("#btn4").addEventListener("click", (e) => {
   e.target.style.backgroundColor = "red";
-  document.querySelector('#btn2').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn3').style.backgroundColor = '#5c67f2';
-  document.querySelector('#btn1').style.backgroundColor = '#5c67f2';
-} );
+  document.querySelector("#btn2").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn3").style.backgroundColor = "#5c67f2";
+  document.querySelector("#btn1").style.backgroundColor = "#5c67f2";
+});
 
-function allCompleted(){
+function allCompleted() {
   const listContainer = document.getElementById("all-task");
   listContainer.innerHTML = "";
-  for(let i=0; i<arrayList.length; i++)
-  {
-    if(arrayList[i] !== undefined && arrayList[i].marked)
+  // for(let i=0; i<arrayList.length; i++)
+  // {
+  //   if(arrayList[i] !== undefined && arrayList[i].marked)
+  //   {
+  //     let li = document.createElement("li");
+  //     li.innerHTML = arrayList[i].task;
+  //     li.setAttribute("id" , `${arrayList[i].id}`);
+  //     li.style.backgroundColor = '#C0E49A';
+  //     listContainer.appendChild(li);
+  //   }
+  // }
+  console.log('-------');
+  for (let i = 0; i < arrayList.length; i++) {
+    if (arrayList[i] === undefined) {
+      continue;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = arrayList[i].task;
+    li.setAttribute("id", `${arrayList[i].id}`);
+    if (arrayList[i].marked) {
+      li.style.backgroundColor = "#C0E49A";
+    }
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    li.appendChild(deleteBtn);
+    console.log(arrayList[i].task);
+    if(arrayList[i].marked)
     {
-      let li = document.createElement("li");
-      li.innerHTML = arrayList[i].task;
-      li.setAttribute("id" , `${arrayList[i].id}`);
-      li.style.backgroundColor = '#C0E49A';
       listContainer.appendChild(li);
+    }
+
+    li.querySelector("button").addEventListener("click", remove);
+    function remove() {
+      for (j = 0; j < arrayList.length; j++) {
+        //console.log(arrayList[j].id , Number(li.getAttribute("id")));
+        if (
+          arrayList[j] !== undefined &&
+          arrayList[j].id === Number(li.getAttribute("id"))
+        ) {
+          delete arrayList[j];
+          break;
+        }
+      }
+      li.remove();
     }
   }
 }
 
-function remaining(){
+function remaining() {
   const listContainer = document.getElementById("all-task");
   listContainer.innerHTML = "";
-  for(let i=0; i<arrayList.length; i++)
-  {
-    if(arrayList[i] !== undefined && arrayList[i].marked === false)
+  // for (let i = 0; i < arrayList.length; i++) {
+  //   if (arrayList[i] !== undefined && arrayList[i].marked === false) {
+  //     let li = document.createElement("li");
+  //     li.innerHTML = arrayList[i].task;
+  //     li.setAttribute("id", `${arrayList[i].id}`);
+  //     listContainer.appendChild(li);
+  //   }
+  // }
+  for (let i = 0; i < arrayList.length; i++) {
+    if (arrayList[i] === undefined) {
+      continue;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = arrayList[i].task;
+    li.setAttribute("id", `${arrayList[i].id}`);
+    if (arrayList[i].marked) {
+      li.style.backgroundColor = "#C0E49A";
+    }
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    li.appendChild(deleteBtn);
+    console.log(arrayList[i].task);
+    if(!arrayList[i].marked)
     {
-      let li = document.createElement("li");
-      li.innerHTML = arrayList[i].task;
-      li.setAttribute("id" , `${arrayList[i].id}`);
       listContainer.appendChild(li);
+    }
+
+    li.querySelector("button").addEventListener("click", remove);
+    function remove() {
+      for (j = 0; j < arrayList.length; j++) {
+        //console.log(arrayList[j].id , Number(li.getAttribute("id")));
+        if (
+          arrayList[j] !== undefined &&
+          arrayList[j].id === Number(li.getAttribute("id"))
+        ) {
+          delete arrayList[j];
+          break;
+        }
+      }
+      li.remove();
     }
   }
 }
 
-function clearCompleted(){
+function clearCompleted() {
   const listContainer = document.getElementById("all-task");
   listContainer.innerHTML = "";
-  for(let i=0; i<arrayList.length; i++)
-  {
-    if(arrayList[i] !== undefined && arrayList[i].marked)
-    {
+  for (let i = 0; i < arrayList.length; i++) {
+    if (arrayList[i] !== undefined && arrayList[i].marked) {
       delete arrayList[i];
     }
   }
-  for(let i=0; i<arrayList.length; i++)
-  {
-    if(arrayList[i] !== undefined)
-    {
-      let li = document.createElement("li");
-      li.innerHTML = arrayList[i].task;
-      li.setAttribute("id" , `${arrayList[i].id}`);
-      listContainer.appendChild(li);
+  for (let i = 0; i < arrayList.length; i++) {
+    if (arrayList[i] === undefined) {
+      continue;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = arrayList[i].task;
+    li.setAttribute("id", `${arrayList[i].id}`);
+    if (arrayList[i].marked) {
+      li.style.backgroundColor = "#C0E49A";
+    }
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    li.appendChild(deleteBtn);
+    console.log(arrayList[i].task);
+    listContainer.appendChild(li);
+
+    li.querySelector("button").addEventListener("click", remove);
+    function remove() {
+      for (j = 0; j < arrayList.length; j++) {
+        //console.log(arrayList[j].id , Number(li.getAttribute("id")));
+        if (
+          arrayList[j] !== undefined &&
+          arrayList[j].id === Number(li.getAttribute("id"))
+        ) {
+          delete arrayList[j];
+          break;
+        }
+      }
+      li.remove();
     }
   }
 }
 
-function showAll(){
+function showAll() {
   taskAdd(2);
 }
-
